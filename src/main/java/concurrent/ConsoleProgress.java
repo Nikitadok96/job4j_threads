7880dc3e494453f0eea4ec204c0b1e0d369b16b8
Mingrow <nikitosvozh@gmail.com>
2023-10-01 21:00:05 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        int count = 0;
+        var process = new char[] {'-', '\\', '|', '/'};
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                int index = count % process.length;
+                System.out.print("\r load: " + process[index]);
+                count++;
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress  = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
