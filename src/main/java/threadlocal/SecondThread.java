ae0d2617602cbce3b4b7af601fcd8a3acafebd8f
Mingrow <nikitosvozh@gmail.com>
2023-10-02 07:48:35 +0300

    11. Отладка в многопоточном приложении [#504954 #449447]

---
+++ src/main/java/threadlocal/SecondThread.java
@@ -0,0 +1,9 @@
+package threadlocal;
+
+public class SecondThread extends Thread {
+    @Override
+    public void run() {
+        ThreadLocalDemo.tl.set("Это поток 2.");
+        System.out.println(ThreadLocalDemo.tl.get());
+    }
+}
