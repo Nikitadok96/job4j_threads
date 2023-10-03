172bdd15fdb28d3a2de14b2897db21b002580599
Mingrow <nikitosvozh@gmail.com>
2023-10-03 07:43:26 +0300

    8. Скачивание файла с ограничением. [#144271 #449442]

--- src/main/java/concurrent/Wget.java
+++ src/main/java/concurrent/Wget.java
@@ -7,35 +7,46 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.sql.Time;
 import java.util.regex.Pattern;
 
 public class Wget implements Runnable {
     private final String url;
+    private final String fileTemp;
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String fileTemp, int speed) {
         this.url = url;
+        this.fileTemp = fileTemp;
         this.speed = speed;
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        var file = new File("tmp.xml");
+        var file = new File(fileTemp);
         try (var in = new URL(url).openStream();
              var out = new FileOutputStream(file)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
             var dataBuffer = new byte[512];
             int bytesRead;
+            var downloadAt = System.nanoTime();
+            int size = 0;
             while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
                 out.write(dataBuffer, 0, bytesRead);
-                double nanoTime = System.nanoTime() - downloadAt;
-                System.out.println("Time: " + nanoTime);
-                System.out.println("Read 512 bytes : " + nanoTime + " nano.");
-                int currentTime = (int) ((512.0 / nanoTime) * 1000);
-                if (currentTime * 1000 > speed) {
-                    Thread.sleep(currentTime);
+                size += dataBuffer.length;
+                if (size >= speed) {
+                    var nanoTime = System.nanoTime() - downloadAt;
+                    System.out.println("Read: " + size + " byte : " + nanoTime + " nanoSec");
+                    long byteSec = (long) (((double) size / nanoTime) * 1000000);
+                    System.out.println("ByteSec: " + byteSec);
+                    if (byteSec  >= speed) {
+                        long needSleep = byteSec / 1000;
+                        System.out.println("Need sleep: " + needSleep);
+                        Thread.sleep(needSleep);
+                    }
+                    size = 0;
+                    downloadAt = System.nanoTime();
                 }
             }
             System.out.println(Files.size(file.toPath()) + " bytes");
@@ -49,22 +60,25 @@ public class Wget implements Runnable {
         validate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, args[2], speed));
         wget.start();
         wget.join();
     }
 
     private static void validate(String[] args) {
-        if (args.length != 2) {
+        if (args.length != 3) {
             throw new IllegalArgumentException("Arguments not passed to program");
         }
-        if (!Pattern.compile("^https://\\w+|.xml$").matcher(args[0]).find()) {
+        if (!Pattern.compile("^https://\\w+|.\\w+$").matcher(args[0]).find()) {
             throw new IllegalArgumentException(String.format(
                     "Error: This argument '%s' is not a link", args[0]
             ));
         }
         if (Integer.parseInt(args[1]) < 0) {
             throw new IllegalArgumentException("The speed must be greater than zero");
+        }
+        if (!Pattern.compile("\\w.\\w+$").matcher(args[2]).find()) {
+            throw new IllegalArgumentException("No data storage file specified");
         }
     }
 }
