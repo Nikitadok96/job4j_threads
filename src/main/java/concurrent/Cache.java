545445ecc0dc760960a6b99943ee0ccce3416d2f
Mingrow <nikitosvozh@gmail.com>
2023-10-04 06:36:27 +0300

    1. Синхронизация общих ресурсов. [#1096]

--- src/main/java/concurrent/Cache.java
+++ src/main/java/concurrent/Cache.java
@@ -3,7 +3,7 @@ package concurrent;
 public class Cache {
     private static Cache cache;
 
-    public static Cache instOf() {
+    public synchronized static Cache instOf() {
         if (cache == null) {
             cache = new Cache();
         }
