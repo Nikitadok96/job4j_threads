0ba7295f1410adea363fc54a716c5f2a6cde9579
Mingrow <nikitosvozh@gmail.com>
2023-10-09 19:54:39 +0300

    0. Управление нитью через wait. [#6858 #451135]

--- src/main/java/wait/CountBarrier.java
+++ src/main/java/wait/CountBarrier.java
@@ -3,7 +3,6 @@ package wait;
 public class CountBarrier {
     private final Object monitor = this;
     private final int total;
-
     private int count = 0;
 
     public CountBarrier(final int total) {
@@ -27,5 +26,30 @@ public class CountBarrier {
                 }
             }
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CountBarrier countBarrier = new CountBarrier(3);
+        Thread master = new Thread(() -> {
+            System.out.println("Master is started");
+            countBarrier.count();
+        });
+        Thread firstSlave = new Thread(() -> {
+            System.out.println("First slave is started");
+            countBarrier.count();
+            countBarrier.await();
+            System.out.println("First slave is done");
+        });
+        Thread secondSlave = new Thread(() -> {
+            System.out.println("Second slave is started");
+            countBarrier.count();
+            countBarrier.await();
+            System.out.println("Second slave is done");
+        });
+        master.start();
+        Thread.sleep(2000);
+        firstSlave.start();
+        Thread.sleep(2000);
+        secondSlave.start();
     }
 }
