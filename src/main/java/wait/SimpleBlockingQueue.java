e4fd2a39db1931ce7aea2f0993b6b4846b7f320a
Mingrow <nikitosvozh@gmail.com>
2023-10-09 21:01:05 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/wait/SimpleBlockingQueue.java
@@ -0,0 +1,78 @@
+package wait;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private final int total;
+
+    public SimpleBlockingQueue(int total) {
+        this.total = total;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (queue.size() >= total) {
+            this.wait();
+        }
+        queue.offer(value);
+        this.notifyAll();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        T value;
+        while ((value = queue.poll()) == null) {
+            this.wait();
+        }
+        this.notifyAll();
+        return value;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>(2);
+        Thread firstConsumer = new Thread(() -> {
+            Integer value = null;
+            try {
+                System.out.println("First consumer poll T");
+                value = simpleBlockingQueue.poll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.printf("First consumer get {%d}\n", value);
+        });
+        Thread secondConsumer = new Thread(() -> {
+            Integer value = null;
+            try {
+                firstConsumer.join();
+                System.out.println("Second consumer poll T");
+                value = simpleBlockingQueue.poll();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.printf("Second consumer get {%d}\n", value);
+        });
+        Thread producer = new Thread(() -> {
+            System.out.println("Producer give first value");
+            try {
+                simpleBlockingQueue.offer(1);
+                firstConsumer.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println("Producer give second value");
+            try {
+                simpleBlockingQueue.offer(2);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        firstConsumer.start();
+        secondConsumer.start();
+        producer.start();
+    }
+}
---
+++ src/test/java/wait/SimpleBlockingQueueTest.java
@@ -0,0 +1,67 @@
+package wait;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenAdd1AndGet() throws InterruptedException {
+        SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>(1);
+        Thread producer = new Thread(() -> {
+            try {
+                simpleBlockingQueue.offer(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        producer.start();
+        producer.join();
+        Thread consumer = new Thread(() -> {
+            try {
+                 assertThat(simpleBlockingQueue.poll()).isEqualTo(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+        });
+        consumer.start();
+    }
+
+    @Test
+    public void whenConsistent()  {
+        SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>(2);
+        Thread firstConsumer = new Thread(() -> {
+            try {
+                assertThat(simpleBlockingQueue.poll()).isEqualTo(1);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread secondConsumer = new Thread(() -> {
+            try {
+                firstConsumer.join();
+                assertThat(simpleBlockingQueue.poll()).isEqualTo(2);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        Thread producer = new Thread(() -> {
+            try {
+                simpleBlockingQueue.offer(1);
+                firstConsumer.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            try {
+                simpleBlockingQueue.offer(2);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        firstConsumer.start();
+        secondConsumer.start();
+        producer.start();
+    }
+}
