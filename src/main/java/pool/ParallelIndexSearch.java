b3c8587f181a81d8183bbfeacdddf22b885b1bd2
Mingrow <nikitosvozh@gmail.com>
2023-10-19 19:48:00 +0300

    3. ForkJoinPool [#315067 #452789]

--- src/main/java/pool/ParallelIndexSearch.java
+++ src/main/java/pool/ParallelIndexSearch.java
@@ -1,25 +1,27 @@
 package pool;
 
 import java.util.Arrays;
-import java.util.NoSuchElementException;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
 
     private final T[] array;
     private final T searchObject;
+    private int from;
+    private int to;
 
-    public ParallelIndexSearch(T[] array, T searchObject) {
+    public ParallelIndexSearch(T[] array, T searchObject, int from, int to) {
         this.array = array;
         this.searchObject = searchObject;
+        this.from = from;
+        this.to = to;
     }
 
     @Override
     protected Integer compute() {
         int rsl = -1;
-        if (array.length <= 10) {
-            for (int i = 0; i < array.length; i++) {
+        if (to - from <= 10) {
+            for (int i = from; i < to; i++) {
                 if (array[i].equals(searchObject)) {
                     rsl = i;
                     break;
@@ -27,29 +29,15 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
             }
             return rsl;
         }
-        int mid = array.length / 2;
+        int mid = (from + to) / 2;
         ParallelIndexSearch<T> leftArray =
-                new ParallelIndexSearch<>(Arrays.copyOfRange(array, 0, mid), searchObject);
+                new ParallelIndexSearch<>(array, searchObject, from, mid);
         ParallelIndexSearch<T> rightArray =
-                new ParallelIndexSearch<>(Arrays.copyOfRange(array, mid, array.length), searchObject);
+                new ParallelIndexSearch<>(array, searchObject, mid + 1, to);
         leftArray.fork();
         rightArray.fork();
         int left = leftArray.join();
         int right = rightArray.join();
-        return merge(left, right);
-    }
-
-    private int merge(int left, int right) {
-        int length = array.length / 2;
-        int rsl = 0;
-        if (left > 0) {
-            rsl += left;
-        } else if (right > 0) {
-            if (array.length >= 10) {
-                rsl += length;
-            }
-            rsl += right;
-        }
-        return rsl;
+        return Math.max(left, right);
     }
 }
--- src/test/java/pool/ParallelIndexSearchTest.java
+++ src/test/java/pool/ParallelIndexSearchTest.java
@@ -12,7 +12,8 @@ class ParallelIndexSearchTest {
     @Test
     public void whenIndex12AndLengthMore10() {
         Integer[] array = new Integer[] {11, 3, 14, 15, 16, 17, 18, 19, 20, 11, 1, 10, 13, 4, 5, 6, 7, 8, 9, 12};
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 12;
@@ -22,7 +23,8 @@ class ParallelIndexSearchTest {
     @Test
     public void whenIndex19AndLengthMore10() {
         Integer[] array = new Integer[] {11, 3, 14, 15, 16, 17, 18, 19, 20, 11, 1, 10, 12, 4, 5, 6, 7, 8, 9, 13};
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 19;
@@ -33,7 +35,8 @@ class ParallelIndexSearchTest {
     @Test
     public void whenIndexIs1AndLengthLess10() {
         Integer[] array = new Integer[] {10, 13, 4, 5, 6, 7, 8, 9, 12};
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 1;
@@ -43,7 +46,8 @@ class ParallelIndexSearchTest {
     @Test
     public void whenIndexIs1AndLengthMore10() {
         Integer[] array = new Integer[] {11, 13, 14, 15, 16, 17, 18, 19, 20, 11, 1, 10, 3, 4, 5, 6, 7, 8, 9, 12};
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 13,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 1;
@@ -53,7 +57,8 @@ class ParallelIndexSearchTest {
     @Test
     public void whenSearchStringIndexIs3AndLengthLess10() {
         String[] strings = new String[] {"Name", "Nikita", "Oleg", "Semen", "Vlad"};
-        ParallelIndexSearch<String> parallelIndexSearch = new ParallelIndexSearch<>(strings, "Semen");
+        ParallelIndexSearch<String> parallelIndexSearch = new ParallelIndexSearch<>(strings, "Semen",
+                0, strings.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 3;
@@ -64,7 +69,8 @@ class ParallelIndexSearchTest {
     public void whenSearchStringIndexIs10AndLengthMore10() {
         String[] strings = new String[] {"Name", "Nikita", "Oleg", "Dmitry", "Vlad", "Ivan", "Viktor",
         "Igor", "Stas", "Egor", "Semen"};
-        ParallelIndexSearch<String> parallelIndexSearch = new ParallelIndexSearch<>(strings, "Semen");
+        ParallelIndexSearch<String> parallelIndexSearch = new ParallelIndexSearch<>(strings, "Semen",
+                0, strings.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 10;
@@ -74,7 +80,8 @@ class ParallelIndexSearchTest {
     @Test
     public void whenIndexIs0() {
         Integer[] array = new Integer[] {10, 13, 4, 5, 6, 7, 8, 9, 12};
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 10);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 10,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 0;
@@ -84,7 +91,8 @@ class ParallelIndexSearchTest {
     @Test
     public void whenNotFoundIndex() {
         Integer[] array = new Integer[] {10, 13, 4, 5, 6, 7, 8, 9, 12};
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 22);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 22,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = -1;
@@ -97,7 +105,8 @@ class ParallelIndexSearchTest {
                 11, 3, 14, 15, 16, 17, 18, 19, 20, 11, 1, 10, 13, 4, 5, 6, 7, 8, 9, 12,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
         };
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 23);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 23,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 22;
@@ -110,7 +119,8 @@ class ParallelIndexSearchTest {
                 11, 3, 14, 15, 16, 17, 18, 19, 20, 11, 1, 10, 13, 4, 5, 6, 7, 8, 9, 12,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
         };
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 14);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 14,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 2;
@@ -123,7 +133,8 @@ class ParallelIndexSearchTest {
                 11, 3, 14, 15, 16, 17, 18, 19, 20, 11, 1, 10, 13, 4, 5, 6, 7, 8, 9, 12,
                 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
         };
-        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 10);
+        ParallelIndexSearch<Integer> parallelIndexSearch = new ParallelIndexSearch<>(array, 10,
+                0, array.length);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         int rsl = forkJoinPool.invoke(parallelIndexSearch);
         int expected = 11;
